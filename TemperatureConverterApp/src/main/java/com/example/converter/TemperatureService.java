344984891ad96a0105c03e795f933ab1fe082ee0
muhammadyaseen-lang <muhammad.yaseen@riphah.edu.pk>
2025-10-31 18:33:11 +0500

    Update TemperatureService.java

--- TemperatureConverterApp/src/main/java/com/example/converter/TemperatureService.java
+++ TemperatureConverterApp/src/main/java/com/example/converter/TemperatureService.java
@@ -2,8 +2,8 @@ package com.example.converter;
 
 public class TemperatureService {
 
-    public static double celsiusToFahrenheit(double c) {
-        return (c * 9 / 5) + 32;
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
     }
 
     public static double fahrenheitToCelsius(double fahrenheit) {
